6fbe3549e19e2aa4fa9c9bea37bf47c6182ff4d9
popovskijoleksandr3-lang <popovskijoleksandr3@gmail.com>
2026-05-11 19:09:57 +0300

    Add files via upload

--- ООП.py
+++ ООП.py
@@ -1,10 +1,19 @@
-class Dog:
-    def __init__(self, name): # Тут ми приймаємо ім'я "з вулиці"
-        self.name = name      # Тут ми кажемо: "Це ТВОЄ ім'я, запам'ятай його"
+class Tank:
+    def __init__(self, name,ammo):
+        self.name = name
+        self.ammo = ammo
 
-    def gav(self):
-        # Ми кажемо self.name, щоб собака згадала, як її звати
-        print(f"Привіт, я {self.name}")
+    def Bk(self):
+        if self.ammo > 0:
+             self.ammo = self.ammo - 1
+             print(f"Танк{self.name} вистрілив залишилось {self.ammo} снарядів")
+        else:
+            print("Закінчились снаряди")
 
-dog1 = Dog("Рекс") # Спрацював __init__, тепер self.name = "Рекс"
-dog1.gav()         # Собака дивиться в себе (self) і бачить там "Рекс"
+tank1=Tank("Т80 БВМ",43)
+tank1.Bk()
+tank1.Bk()
+tank1.Bk()
+tank1.Bk()
+
+
