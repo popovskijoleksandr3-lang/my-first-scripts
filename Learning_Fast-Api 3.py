9f04ff45a72cc64b13de43c1934926a756562bb8
popovskijoleksandr3-lang <popovskijoleksandr3@gmail.com>
2026-05-26 15:55:21 +0300

    Add files via upload

---
+++ Learning_Fast-Api 3.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+@app.get("/")
+def car():
+    return {"info": "Car can has 6 wheels"}
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
