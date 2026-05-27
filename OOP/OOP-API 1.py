e5950cc69eb77282dab0230313d07a2d9cd653b9
popovskijoleksandr3-lang <popovskijoleksandr3@gmail.com>
2026-05-27 21:28:43 +0300

    Add files via upload

--- OOP/OOP-API 1.py
+++ OOP/OOP-API 1.py
@@ -3,16 +3,20 @@ try:
     class Cat_fact:
         def __init__(self, fact_text):
             self.fact = fact_text
-
         def show(self):
-            print(f"Interesting fact: {self.fact}")
-
-
+            print("_"*length)
+            print(f"Interesting fact {self.fact}")
+            print("_"*length)
     url = 'https://catfact.ninja/fact'
     with requests.Session() as session:
-        data = session.get(url).json()
-    my_cat_fact = Cat_fact(data['fact'])
-    my_cat_fact.show()
-except Exception as error:
-   print(error)
+       data=session.get(url).json()
+       length = len(data['fact'])
+       my_cat_fact = Cat_fact(data['fact'])
+       my_cat_fact.show()
+except Exception as e:
+    print(e)
+
+
+
+
 
